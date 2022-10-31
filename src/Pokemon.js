75ec244ccc4c246666d166f2415c58bbadcabde3
Muriel Paraire <muriel.paraire@etu.umontpellier.fr>
2022-10-31 17:34:07 +0100

    feat: adding background + class Pokemon

---
+++ src/Pokemon.js
@@ -0,0 +1,62 @@
+export default class Pokemon {
+    constructor(id, name, types, url) {
+        this.name = name;
+        this.id = id;
+        this.types = types;
+        this.url = url;
+    }
+
+    getPokemon(poke) {
+        this.name = poke.name;
+        return this;
+    }
+
+    getBackgroundColor() {
+        console.log(this.types);
+        if (this.types[0].type.name === 'grass')
+            return 'rgb(153, 233, 142)'
+        if (this.types[0].type.name === 'normal')
+            return 'rgb(202, 202, 202)'
+        if (this.types[0].type.name === 'poison')
+            return 'rgb(234, 176, 255)'
+        if (this.types[0].type.name === 'fire')
+            return 'rgb(255, 191, 150)'   
+        if (this.types[0].type.name === 'water')
+            return 'rgb(150, 213, 255)' 
+        if (this.types[0].type.name === 'bug')
+            return 'rgb(100, 172, 98)' 
+        if (this.types[0].type.name === 'flying')
+            return 'rgb(146, 230, 235)' 
+        if (this.types[0].type.name === 'electric')
+            return 'rgb(235, 235, 146)' 
+        if (this.types[0].type.name === 'ground')
+            return 'rgb(236, 211, 141)' 
+        if (this.types[0].type.name === 'fairy')
+            return 'rgb(248, 185, 255)' 
+        if (this.types[0].type.name === 'fighting')
+            return 'rgb(194, 166, 123)' 
+        if (this.types[0].type.name === 'psychic')
+            return 'rgb(200, 149, 203)' 
+        if (this.types[0].type.name === 'rock')
+            return 'rgb(152, 131, 104)' 
+        if (this.types[0].type.name === 'ghost')
+            return 'rgb(169, 114, 182)' 
+        if (this.types[0].type.name === 'ice')
+            return 'rgb(138, 191, 226)' 
+        if (this.types[0].type.name === 'dragon')
+            return 'rgb(209, 76, 76)' 
+        if (this.types[0].type.name === 'dark')
+            return 'rgb(67, 65, 65)' 
+        if (this.types[0].type.name === 'steel')
+            return 'rgb(118, 118, 118)' 
+        return 'black'
+    }
+
+    showPokemon() {
+        let htmlText = "<div id='" + this.name + "'> <img src= '" + this.url + "' alt='photo of the pokemon " + this.name + "'/> <p class='textId'>id : " 
+        + this.id + "</p> <h1> " + this.name + " </h1> </div> <style> #" + this.name + "{ background-color: " + this.getBackgroundColor() + ";} </style>";
+        console.log(htmlText)
+        return htmlText;
+    }
+
+}
--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -4,56 +4,47 @@
 </template>
 
 <script>
+import Pokemon from '../Pokemon.js'
+
 const Pokedex = require("pokeapi-js-wrapper")
 const P = new Pokedex.Pokedex();
 
-class Pokemon {
-  constructor(id, name, types, url) {
-    this.name = name;
-    this.id = id;
-    this.types = types;
-    this.url = url;
-  }
-
-  getPokemon(poke) {
-    this.name = poke.name;
-    return this;
-  }
-
-  showPokemon() {
-    let htmlText = "<img src= '" + this.url + "' alt='photo of the pokemon " + this.name + "'/> <p class='textId'>id : " + this.id + "</p> <h1> " + this.name + " </h1>";
-    console.log(this);
-    console.log(htmlText);
-    return htmlText;
-  }
-}
 
 let interval = {
-  offset: 0,
-  limit: 20,
+    offset: 0,
+    limit: 20,
 }
 
 P.getPokemonsList(interval)
-  .then(function (response) {
-    let pokemons = [];
-    let poke = document.getElementById("pokemonList");
-    for (let counter = 0; counter < response.results.length; counter++) {
-      fetch(response.results[counter].url)
-        .then((data) => data.json())
-        .then((resp) => {
-          console.log(resp);
-          let p = new Pokemon(resp.id, resp.name, resp.types, resp.sprites.other["official-artwork"].front_default);
-          pokemons.push(poke);
-          let li = document.createElement('li');
-          li.innerHTML = p.showPokemon();
-          poke.appendChild(li);
+    .then(function (response) {
+        let pokemons = [];
+        let poke = document.getElementById("pokemonList");
+        console.log(response)
+        for (let counter = 0; counter < response.results.length; counter++) {
+            fetch(response.results[counter].url)
+                .then((data) => data.json())
+                .then((resp) => {
+                    console.log(resp)
+                    let p = new Pokemon(resp.id, resp.name, resp.types, resp.sprites.other["official-artwork"].front_default);
+                    pokemons.push(p);
+                    let li = document.createElement('li');
+                    li.innerHTML = p.showPokemon();
+                    poke.appendChild(li);
+                }
+            )
         }
-        )
+    });
 
-    }
+/*
+    export default {
+    data: () => {
+      return {
+        offset: 0,
+      }
+    },
+    components: {},
+    methods: {}
+  }
+  */
 
-
-
-  });
-    
 </script>
